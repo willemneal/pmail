fdc686042c9a338b3f9270dbcf913e1ec10bd9ee
Tauqir Abdullah <tauqir.abdullah@gmail.com>
2018-02-01 14:34:48 -0500

    Fixed loading js

--- Extension/content.js
+++ Extension/content.js
@@ -1,10 +1,10 @@
 
 var j = document.createElement('script');
-j.src = chrome.extension.getURL('jquery-1.10.2.min.js');
+j.src = chrome.extension.getURL('utils/jquery-1.10.2.min.js');
 (document.head || document.documentElement).appendChild(j);
 
 var g = document.createElement('script');
-g.src = chrome.extension.getURL('gmail.js');
+g.src = chrome.extension.getURL('utils/gmail.js');
 (document.head || document.documentElement).appendChild(g);
 
 var s = document.createElement('script');
