8cdbcdc65fca1449598cf54a29e51de51c14fb74
Tauqir Abdullah <tauqir.abdullah@gmail.com>
2018-02-01 14:27:48 -0500

    Maybe this works

--- Extension/content.js
+++ Extension/content.js
@@ -1,37 +1,12 @@
-var resources =  ['jquery-1.10.2.min.js',
-                  //'jquery-3.0.0.min.js',
-                  //'jquery-3.1.1.min.js',
-                  // 'search.js',
-                  'load.js',
-                  'hashes.js',
-                  'kbpgp-2.0.8.js',
-                  'gmail.js', 
-                  'websocket.js',
-                  // 'index.js',                 
-                  'sjcl.js',
-                  'openpgp.js',
-                  'long.js',
-                  'main.js'
-                  ];
 
-function loadResource(resource_name) {
-  console.log("loading " + resource_name)
-  var script = document.createElement('script');
-  script.src = chrome.extension.getURL(resource_name);
-  (document.head || document.documentElement).appendChild(script);
-}
+var j = document.createElement('script');
+j.src = chrome.extension.getURL('jquery-1.10.2.min.js');
+(document.head || document.documentElement).appendChild(j);
 
-/*
-var choice = confirm("Do you want to use Pmail for this account?");
-if(choice == true){
-  for (i in resources) {
-    loadResource(resources[i]);
-  }  
-}else{
-  alert("Pmail is turned off for this account. Every sent and received email will be in plaintext. Also, you are not able to read encrypted emails.");
-}
-*/
+var g = document.createElement('script');
+g.src = chrome.extension.getURL('gmail.js');
+(document.head || document.documentElement).appendChild(g);
 
-for (i in resources) {
-    loadResource(resources[i]);
-  }
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('main.js');
+(document.head || document.documentElement).appendChild(s);
