6453dfa3e375f7edfc50e72708845116915dabc3
Tauqir Abdullah <tauqir.abdullah@gmail.com>
2018-02-15 16:00:21 -0500

    testing openpgpjs and gmailjs

--- Extension/main.js
+++ Extension/main.js
@@ -14,45 +14,92 @@ var main = function(){
   // NOTE: Always use the latest version of gmail.js from
   // https://github.com/KartikTalwar/gmail.js
   var openpgp = window.openpgp;
+  var hkp = new openpgp.HKP('https://pgp.mit.edu');
+
   gmail = new Gmail();
   console.log('Hello,', gmail.get.user_email());
 
-  var userPassphrase = "";
-  var userEmail = gmail.get.user_email();
-  
+  var user = {
+    passphrase = "",
+    publickey = "",
+    privatekey = "",
+    email = gmail.get.user_email()
+  };
+
 
   if (localStorage.getItem("pmail.privkey") == null || localStorage.getItem("pmail.pubkey") == null){
-    userPassphrase = prompt("Pmail Account set up \n Please Enter a passphrase to secure your private key: ");  
-    
-    var options = {
-      userIds: [{email: userEmail}], // multiple user IDs
-      numBits: 4096,                 // RSA key size
-      passphrase: userPassphrase     // protects the private key
-    };
-    
-    openpgp.generateKey(options).then(function(key) {
-      var privkey = key.privateKeyArmored; // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
-      var pubkey = key.publicKeyArmored;   // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
 
-      localStorage.setItem("pmail.privkey", privkey); 
-      localStorage.setItem("pmail.pubkey", pubkey);
-    });
-    
-    
+    user.passphrase = prompt("Pmail Account set up (If importing a private key leave response blank)\n Please Enter a passphrase to secure your private key: ");  
 
+    if(user.passphrase == ""){
+
+      user.privatekey = prompt("Importing a private key? \n Please your private key: ");  
+      localStorage.setItem("pmail.privkey", user.privatekey); 
+
+      user.passphrase = prompt("Please enter the passphrase used to secure your private key: "); 
+      
+      var options = {
+          query: (user.email+' pmail')
+      };
+
+      hkp.lookup(options).then(function(key) {
+          user.publickey = openpgp.key.readArmored(key);
+          localStorage.setItem("pmail.pubkey", user.publickey); 
+
+      });
+
+    } else {
+      
+      var options = {
+        userIds: [{email: user.email , name: "pmailtest"}], // multiple user IDs
+        numBits: 4096,                                 // RSA key size
+        passphrase: user.passphrase                     // protects the private key
+      };
+      
+      openpgp.generateKey(options).then(function(key) {
+        user.privatekey = key.privateKeyArmored; // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
+        user.publickey = key.publicKeyArmored;   // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+
+        hkp.upload(user.publickey).then(function() {  });
+
+
+        localStorage.setItem("pmail.privkey", user.privatekey); 
+        localStorage.setItem("pmail.pubkey", user.publickey);
+      });
+  }
+    
   } else {
-
-    userPassphrase = prompt("Please Enter your passphrase: ");
+    user.passphrase = prompt("Please Enter your passphrase: ");
+    user.privatekey = localStorage.getItem("pmail.privkey");
+    user.publickey = localStorage.getItem("pmail.publickey")
   }
   
+  console.log(user);
 
-  
-  var userPublicKey = "";
-  var userPrivateKey = "";
+
 
   
   gmail.observe.before('send_message', function(url, body, data, xhr){
     console.log("url:", url, 'body', body, 'email_data', data, 'xhr', xhr);  
+    var oldCmml = xhr.xhrParams.url.cmml;
+
+    var body_params = xhr.xhrParams.body_params;
+    var string = '<div dir="ltr"><div>hello</div></div>';
+
+    if (string.length > oldCmml) {
+        xhr.xhrParams.url.cmml = string.length;
+    } else {
+        string += '<div>';
+        while(string.length<oldCmml){
+            string += ' ';
+        }
+        string += '</div>';
+        xhr.xhrParams.url.cmml = string.length;
+    }
+
+    body_params.body = string;
+
+    console.log(oldCmml, xhr.xhrParams.url.cmml, string);
   });
   
   gmail.observe.on("open_email", function(id, url, body, xhr) {
