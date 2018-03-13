168b8d0514209cc46bfe5b403e677c9a953122d9
Tauqir Abdullah <tauqir.abdullah@gmail.com>
2018-03-13 12:41:58 -0400

    Server with boltDB working for PublicKey (For testing)

--- Server/main.go
+++ Server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,35 +10,54 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//User
+type User struct {
+	Email     string `json:",email"`
+	Publickey string `json:",publickey"`
+}
+
 var publicKeysBucket = []byte("PublicKeys")
 var db *bolt.DB
 
+//GetPublicKey with the param email
 func GetPublicKey(w http.ResponseWriter, r *http.Request) {
 	email := []byte(r.FormValue("email"))
 	db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket(publicKeysBucket)
 		v := b.Get(email)
-		fmt.Printf("The answer is: %s\n", v)
+		user := User{Email: string(email[:]), Publickey: string(v[:])}
+		json.NewEncoder(w).Encode(user)
 		return nil
 	})
+
 }
 
+//AddPublicKey adds a the public key with email as the key
 func AddPublicKey(w http.ResponseWriter, r *http.Request) {
 	email := []byte(r.FormValue("email"))
 	publicKey := []byte(r.FormValue("publicKey"))
 	db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(publicKeysBucket)
 		err := b.Put(email, publicKey)
-		return err
+		user := User{Email: string(email[:]), Publickey: string(publicKey[:])}
+		if err == nil {
+			json.NewEncoder(w).Encode(user)
+		}
+		return nil
 	})
 }
 
+//RemovePublicKey removes public key with email as the key
 func RemovePublicKey(w http.ResponseWriter, r *http.Request) {
 	email := []byte(r.FormValue("email"))
 	db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(publicKeysBucket)
 		err := b.Delete(email)
-		return err
+		user := User{Email: string(email[:]), Publickey: "DELETED"}
+		if err == nil {
+			json.NewEncoder(w).Encode(user)
+		}
+		return nil
 	})
 
 }
@@ -46,12 +66,13 @@ func RemovePublicKey(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := mux.NewRouter()
 
-	db, err := bolt.Open("pmail.db", 0600, nil)
+	dbO, err := bolt.Open("pmail.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	defer dbO.Close()
 
+	db = dbO
 	db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists(publicKeysBucket)
 		if err != nil {
